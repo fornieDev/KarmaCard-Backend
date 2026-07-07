eafd6a5efde7eb25eb519fd5ba429c98cf82b2bc
fornieDev <jfornieortega@gmail.com>
2026-07-07 09:48:38 +0200

    Initial commit

---
+++ src/main/java/com/forniedev/karmacard/KarmacardApplication.java
@@ -0,0 +1,13 @@
+package com.forniedev.karmacard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KarmacardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KarmacardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/forniedev/karmacard/KarmacardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forniedev.karmacard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KarmacardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
